c2f9d45340efc912ac1ae7936b86881620373740
Parsa Sharifi <sharifidev@outlook.com>
2023-07-05 14:34:04 +0330

    Add more colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,5 +2,11 @@ import 'package:flutter/material.dart';
 
 class Kcolor {
   static const Color white = Color(0xFFFFFFFF);
-  static const Color blueIndicator = Color(0xFF3B5EDF);
+  static const Color black = Color(0xFF000000);
+  static const Color background = Color(0xFFEEEEEE);
+  static const Color primery = Color(0xFF3B5EDF);
+  static const Color secondary = Color(0xFF1DB68B);
+  static const Color tertiary = Color(0xFFD02026);
+  static const Color grey = Color(0xFF858585);
+  static const Color grey2 = Color(0xFFEEEEEE);
 }
