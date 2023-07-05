7d54a33016fd684f9ac6b996ab9469e466fca129
Parsa Sharifi <sharifidev@outlook.com>
2023-07-05 14:35:41 +0330

    fluttergen's files

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,177 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/apple.svg
+  String get apple => 'assets/icons/apple.svg';
+
+  /// File path: assets/icons/arrow_left.svg
+  String get arrowLeft => 'assets/icons/arrow_left.svg';
+
+  /// File path: assets/icons/arrow_right.svg
+  String get arrowRight => 'assets/icons/arrow_right.svg';
+
+  /// File path: assets/icons/arrow_right_bold.svg
+  String get arrowRightBold => 'assets/icons/arrow_right_bold.svg';
+
+  /// File path: assets/icons/category.svg
+  String get category => 'assets/icons/category.svg';
+
+  /// File path: assets/icons/category_bold.svg
+  String get categoryBold => 'assets/icons/category_bold.svg';
+
+  /// File path: assets/icons/checkout.svg
+  String get checkout => 'assets/icons/checkout.svg';
+
+  /// File path: assets/icons/checkout_bold.svg
+  String get checkoutBold => 'assets/icons/checkout_bold.svg';
+
+  /// File path: assets/icons/delete.svg
+  String get delete => 'assets/icons/delete.svg';
+
+  /// File path: assets/icons/favorite.svg
+  String get favorite => 'assets/icons/favorite.svg';
+
+  /// File path: assets/icons/home.svg
+  String get home => 'assets/icons/home.svg';
+
+  /// File path: assets/icons/home_bold.svg
+  String get homeBold => 'assets/icons/home_bold.svg';
+
+  /// File path: assets/icons/option.svg
+  String get option => 'assets/icons/option.svg';
+
+  /// File path: assets/icons/profile.svg
+  String get profile => 'assets/icons/profile.svg';
+
+  /// File path: assets/icons/profile_bold.svg
+  String get profileBold => 'assets/icons/profile_bold.svg';
+
+  /// File path: assets/icons/search.svg
+  String get search => 'assets/icons/search.svg';
+
+  /// File path: assets/icons/star.svg
+  String get star => 'assets/icons/star.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        apple,
+        arrowLeft,
+        arrowRight,
+        arrowRightBold,
+        category,
+        categoryBold,
+        checkout,
+        checkoutBold,
+        delete,
+        favorite,
+        home,
+        homeBold,
+        option,
+        profile,
+        profileBold,
+        search,
+        star
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/iphone.png
+  AssetGenImage get iphone => const AssetGenImage('assets/images/iphone.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [iphone];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,33 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: GB
+  static const String gb = 'GB';
+
+  /// Font family: GEB
+  static const String geb = 'GEB';
+
+  /// Font family: GH
+  static const String gh = 'GH';
+
+  /// Font family: GM
+  static const String gm = 'GM';
+
+  /// Font family: GR
+  static const String gr = 'GR';
+
+  /// Font family: SB
+  static const String sb = 'SB';
+
+  /// Font family: SM
+  static const String sm = 'SM';
+}
