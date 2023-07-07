aea5a04d79617c4ba165545180c4c7db03e860a1
Parsa Sharifi <sharifidev@outlook.com>
2023-07-07 21:15:09 +0330

    Add a glass NavigationBar to Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'utility/svg.dart';
+import 'gen/assets.gen.dart';
 import 'pages/home_page.dart';
+import 'constants/colors.dart';
+import 'pages/profile_page.dart';
+import 'pages/category_page.dart';
+import 'pages/productlist_page.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int selectedBottomNavigationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true, fontFamily: 'SM'),
       localizationsDelegates: const [
         GlobalCupertinoLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -22,7 +35,144 @@ class MainApp extends StatelessWidget {
       ],
       supportedLocales: const [Locale("fa", "IR")],
       locale: const Locale("fa", "IR"),
-      home: const HomePage(),
+      home: Scaffold(
+        body: IndexedStack(
+          index: selectedBottomNavigationIndex,
+          children: getPages(),
+        ),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+            child: BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  selectedBottomNavigationIndex = index;
+                });
+              },
+              selectedFontSize: 12,
+              unselectedFontSize: 12,
+              selectedItemColor: Kcolor.primery,
+              unselectedItemColor: Kcolor.black,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.transparent,
+              currentIndex: selectedBottomNavigationIndex,
+              selectedLabelStyle: const TextStyle(fontFamily: 'SB'),
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Ksvg(
+                    size: 26,
+                    color: Kcolor.black,
+                    path: Assets.icons.home,
+                  ),
+                  label: 'خانه',
+                  activeIcon: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 20,
+                          spreadRadius: -7,
+                          offset: Offset(0, 10),
+                          color: Kcolor.primery,
+                        )
+                      ],
+                    ),
+                    child: Ksvg(
+                      size: 26,
+                      color: Kcolor.primery,
+                      path: Assets.icons.homeBold,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: Ksvg(
+                    size: 26,
+                    color: Kcolor.black,
+                    path: Assets.icons.category,
+                  ),
+                  label: 'دسته بندی ها',
+                  activeIcon: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 20,
+                          spreadRadius: -7,
+                          offset: Offset(0, 10),
+                          color: Kcolor.primery,
+                        )
+                      ],
+                    ),
+                    child: Ksvg(
+                      size: 26,
+                      color: Kcolor.primery,
+                      path: Assets.icons.categoryBold,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: Ksvg(
+                    path: Assets.icons.checkout,
+                    size: 26,
+                    color: Kcolor.black,
+                  ),
+                  label: 'سبد خرید',
+                  activeIcon: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 20,
+                          spreadRadius: -7,
+                          offset: Offset(0, 10),
+                          color: Kcolor.primery,
+                        )
+                      ],
+                    ),
+                    child: Ksvg(
+                      size: 26,
+                      color: Kcolor.primery,
+                      path: Assets.icons.checkoutBold,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: Ksvg(
+                    size: 26,
+                    color: Kcolor.black,
+                    path: Assets.icons.profile,
+                  ),
+                  label: 'پروفایل',
+                  activeIcon: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 20,
+                          spreadRadius: -7,
+                          offset: Offset(0, 10),
+                          color: Kcolor.primery,
+                        )
+                      ],
+                    ),
+                    child: Ksvg(
+                      size: 26,
+                      color: Kcolor.primery,
+                      path: Assets.icons.profileBold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  List<Widget> getPages() {
+    return [
+      const HomePage(),
+      const CategoryPage(),
+      const ProductListPage(),
+      const ProfilePage()
+    ];
   }
 }
