d1815945e17a3dc4281160d641d331b7ca214e9c
Parsa Sharifi <sharifidev@outlook.com>
2023-07-07 21:16:07 +0330

    Create base of ProfilePage

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,20 @@
+import 'package:apple_shop/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Kcolor.background,
+      body: SafeArea(
+        child: Column(
+          children: [
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
