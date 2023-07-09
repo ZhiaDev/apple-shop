9302838bbfae475fbf6ac42fa1889f5cf5757ca3
Parsa Sharifi <sharifidev@outlook.com>
2023-07-09 23:49:03 +0330

    Create base of ProductDetailPage

---
+++ lib/pages/productdetail_page.dart
@@ -0,0 +1,18 @@
+import 'package:apple_shop/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  const ProductDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Kcolor.background,
+      body: CustomScrollView(
+        slivers: [
+          //
+        ],
+      ),
+    );
+  }
+}
