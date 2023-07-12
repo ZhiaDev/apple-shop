2b450fcde9d37910006c15bdbc0b2c92711f00d1
Parsa Sharifi <sharifidev@outlook.com>
2023-07-12 19:10:06 +0330

    Create base of CheckoutPage

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '/constants/colors.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Kcolor.background,
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
