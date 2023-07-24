398708697305f8449ad08595c2123ded28b05863
Parsa Sharifi <sharifidev@outlook.com>
2023-07-24 12:01:42 +0330

    Create Product Bloc

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -4,19 +4,26 @@ import '/di/di.dart';
 import 'home_event.dart';
 import 'home_state.dart';
 import '/data/repository/banner_repository.dart';
+import '/data/repository/product_repository.dart';
 import '/data/repository/category_repository.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final IBannerRepository _bannerRepository = locator.get();
   final ICategoryRepository _categoryRepository = locator.get();
+  final IProductRepository _productRepository = locator.get();
 
   HomeBloc() : super(HomeInitState()) {
     on<HomeGetwInitializedData>((event, emit) async {
       emit(HomeLoadingState());
       var bannerList = await _bannerRepository.getBanners();
       var categoryList = await _categoryRepository.getCategories();
+      var productList = await _productRepository.getProducts();
 
-      emit(HomeRequestSuccessState(bannerList, categoryList));
+      emit(HomeRequestSuccessState(
+        bannerList,
+        categoryList,
+        productList,
+      ));
     });
   }
 }
--- lib/bloc/home/home_state.dart
+++ lib/bloc/home/home_state.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 
 import '/data/model/banner.dart';
 import '/data/model/category.dart';
+import '../../data/model/product.dart';
 
 abstract class HomeState {}
 
@@ -12,6 +13,11 @@ class HomeLoadingState extends HomeState {}
 class HomeRequestSuccessState extends HomeState {
   Either<String, List<CampaignBanner>> bannerList;
   Either<String, List<Category>> categoryList;
+  Either<String, List<Product>> productList;
 
-  HomeRequestSuccessState(this.bannerList, this.categoryList);
+  HomeRequestSuccessState(
+    this.bannerList,
+    this.categoryList,
+    this.productList,
+  );
 }
